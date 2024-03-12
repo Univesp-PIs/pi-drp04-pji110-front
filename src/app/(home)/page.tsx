5aca25491c7fefe128e27e52f6c4ca0d18a0515c
Gabriel Ribeiro <gabrielalmeira100@gmail.com>
2024-03-11 22:08:03 -0300

    feat: initial config and create page home

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,40 @@
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <main className="w-full">
+      <div className="w-full flex justify-center py-16" id="quem-somos">
+        <div className="max-w-screen-md w-full flex flex-col gap-4 items-center px-4 md:px-0">
+          <h2 className="text-xl font-bold">Bem-vindo ao Curriculum42</h2>
+          <div className="w-full flex justify-end">
+            <Link
+              href=""
+              className="border py-2 px-8 rounded-md hover:bg-white hover:text-black duration-300"
+            >
+              Login
+            </Link>
+          </div>
+          <div className="border rounded-md p-4 w-full text-center">
+            Apresentação do site/sistema
+          </div>
+          <div className="border rounded-md p-4 w-full text-center">
+            <div className="p-4 w-full flex flex-col gap-4">
+              <div className="border rounded-md p-4 w-full text-center">
+                Informações
+              </div>
+              <div className="border rounded-md p-4 w-full text-center">
+                Objetivos
+              </div>
+              <div className="border rounded-md p-4 w-full text-center">
+                Demonstações (print)
+              </div>
+              <div className="border rounded-md p-4 w-full text-center">
+                Créditos/contato
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  )
+}
