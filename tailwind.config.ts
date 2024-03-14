b4a51c067554009441808e9b9656641afef472df
Gabriel Ribeiro <gabriel.hsribeiro19@gmail.com>
2024-03-14 20:46:48 -0300

    Feature create screen login and dashboard (#4)
    * Create Register modal
    
    * feat: create back button
    
    * fix: remove select color
    
    * Revert "fix: remove select color"
    
    This reverts commit 77b0d5243c05c05424fb3e122abd0b7f049d8800.
    
    * Reapply "fix: remove select color"
    
    This reverts commit 5c6c103864eb54a85dfa3a73fd096d80733b02fb.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss'
+import { violet, blackA, mauve, green } from '@radix-ui/colors'
 
 const config: Config = {
   content: [
@@ -11,6 +12,27 @@ const config: Config = {
       colors: {
         primary: '',
         secondary: '',
+        ...mauve,
+        ...violet,
+        ...green,
+        ...blackA,
+      },
+      keyframes: {
+        overlayShow: {
+          from: { opacity: '0' },
+          to: { opacity: '1' },
+        },
+        contentShow: {
+          from: {
+            opacity: '0',
+            transform: 'translate(-50%, -48%) scale(0.96)',
+          },
+          to: { opacity: '1', transform: 'translate(-50%, -50%) scale(1)' },
+        },
+      },
+      animation: {
+        overlayShow: 'overlayShow 150ms cubic-bezier(0.16, 1, 0.3, 1)',
+        contentShow: 'contentShow 150ms cubic-bezier(0.16, 1, 0.3, 1)',
       },
     },
   },
